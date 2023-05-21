50796a3534df998792f84f841231a81c8d54f90a
Filip Krzemień <filipos2003@gmail.com>
2023-05-21 23:30:22 +0200

    Modal UI component is working

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,24 @@
 import classes from './Header.module.scss';
+import Modal from '../UI/Modal';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [isFormActive , setIsFormActive] = useState(false);
+
+    const closeModalHandler = () => {
+        setIsFormActive(false);
+    }
+
+    const openModalHandler = () => {
+        setIsFormActive(true);
+    }
 
     return(
         <header className={classes.header}>
             <span>Kanban</span>
-            <button className={classes.adder}>Add new task</button>
+            <button className={classes.adder} onClick={openModalHandler}>Add new task</button>
+            {isFormActive && <Modal onClose={closeModalHandler}>Test</Modal>}
         </header>
     )
 
--- src/Components/UI/Modal.tsx
+++ src/Components/UI/Modal.tsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import { createPortal } from 'react-dom';
-import Props from '../models/children';
-import classes from './Modal.module.scss';
+import React from "react";
+import { createPortal } from "react-dom";
+import Props from "../models/children";
+import classes from "./Modal.module.scss";
 
+const Backdrop: React.FC<{ onClose: () => void }> = (props) => {
+  return <div className={classes.backdrop} onClick={() => props.onClose()} />;
+};
 
+const ModalOverlay: React.FC<Props> = (props) => {
+  return <div className={classes.modal}>{props.children}</div>;
+};
 
-const Backdrop = () => {
-    return(
-        <div className={classes.backdrop}></div>
-    )
-}
+const portalEl = document.getElementById("overlays") as HTMLElement;
 
-
-const ModalOverlay:React.FC<Props> = (props) => {
-    return(
-    <div className={classes.modal}>
-        {props.children}
-    </div>
-    );
-}
-
-const portalEl = document.getElementById('overlays') as HTMLElement; 
-
-
-const Modal = () => {
-    return(
-        <React.Fragment>
-            {createPortal(<Backdrop /> , portalEl)}
-            {createPortal(<ModalOverlay /> , portalEl)}
-        </React.Fragment>
-    )
-}
+const Modal: React.FC<{ onClose: () => void; children?: React.ReactNode }> = (
+  props
+) => {
+  return (
+    <React.Fragment>
+      {createPortal(
+        <Backdrop
+          onClose={() => {
+            props.onClose();
+          }}
+        />,
+        portalEl
+      )}
+      {createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalEl)}
+    </React.Fragment>
+  );
+};
 
 export default Modal;
