8fae9c38c3716f62bab517597966229287a3ebf3
Filip Krzemień <filipos2003@gmail.com>
2023-05-23 03:55:13 +0200

    100 percent working new task component

--- src/Components/NewTask/NewTask.tsx
+++ src/Components/NewTask/NewTask.tsx
@@ -3,79 +3,113 @@ import Cart from "../UI/Cart";
 import Subtask from "./Subtask";
 import { Subtask as subtaskModel } from "../models/Subtask";
 import { Select, MenuItem } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 import classes from "./NewTask.module.scss";
 
-
-const NewTask:React.FC<{onClose:()=>void}> = (props) => {
-
+const NewTask: React.FC<{ onClose: () => void }> = (props) => {
+  
   const [selectValue, setSelectValue] = useState<string>("todo");
-  const [subtaks , setSubtask] = useState<subtaskModel[]>([]);
+  const [subtaks, setSubtask] = useState<subtaskModel[]>([]);
+  const [taskNameValue, setTaskNameValue] = useState<string>("");
+  const [decriptionValue, setDecriptionValue] = useState<string>("");
 
   const slectChangeHandler = (event: any) => {
     setSelectValue(event.target.value);
   };
 
-  const addSubtaskHandler = () => {
-    const newSubtasksArray = [...subtaks];
-    newSubtasksArray.push({id:Math.random().toString() , value:''});
-    setSubtask(newSubtasksArray);
-  }
+  const taskChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskNameValue(event.target.value);
+  };
 
-  const subtaskChangeHandler = (value:string , id:string) => {
-   const changedArray = subtaks.map(item => {
-    if(item.id === id){
+  const descriptionChangeHandler = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDecriptionValue(event.target.value);
+  };
+
+  const subtaskChangeHandler = (value: string, id: string) => {
+    const changedArray = subtaks.map((item) => {
+      if (item.id === id) {
         item.value = value;
         return item;
-    }else {
+      } else {
         return item;
-    }
-   });
-  setSubtask(changedArray);
-  console.log(subtaks);
-   }
+      }
+    });
+    setSubtask(changedArray);
+  };
 
-   const delateSubtaskHandler = (id:string) => {
-    const newSubtasks = subtaks.filter(subtask => subtask.id !== id);
+  const addSubtaskHandler = () => {
+    const newSubtasksArray = [...subtaks];
+    newSubtasksArray.push({ id: Math.random().toString(), value: "" });
+    setSubtask(newSubtasksArray);
+  };
+
+  const delateSubtaskHandler = (id: string) => {
+    const newSubtasks = subtaks.filter((subtask) => subtask.id !== id);
     setSubtask(newSubtasks);
-   }
+  };
 
-   const closeModalHandler = () => {
+  const closeModalHandler = () => {
     props.onClose();
-   }
-  
+  };
+
+  const submitFormHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
 
   return (
     <Cart>
-      <form className={classes.form}>
-        <CloseIcon className={classes.close} fontSize="large" onClick = {closeModalHandler} />
+      <form className={classes.form} onSubmit={submitFormHandler}>
+        <CloseIcon
+          className={classes.close}
+          fontSize="large"
+          onClick={closeModalHandler}
+        />
         <h2>Add New Task</h2>
         <div className={classes["form-element"]}>
           <label htmlFor="taskName">Task Name</label>
-          <input type="text" id="taskName" />
+          <input
+            type="text"
+            id="taskName"
+            value={taskNameValue}
+            onChange={taskChangeHandler}
+          />
         </div>
         <div className={classes["form-element"]}>
           <label htmlFor="description">Description</label>
-          <textarea name="text" id="decription" cols={30} rows={7} />
+          <textarea
+            name="text"
+            id="decription"
+            cols={30}
+            rows={7}
+            value={decriptionValue}
+            onChange={descriptionChangeHandler}
+          />
         </div>
-        <div className={`${classes['form-element']} ${classes.subtasks} `}>
-        <label htmlFor="subtasks">Subtasks</label>
-        {subtaks.map(subtaks => {
-            return(
-                <Subtask
-                id = {subtaks.id}
+        <div className={`${classes["form-element"]} ${classes.subtasks} `}>
+          <label htmlFor="subtasks">Subtasks</label>
+          {subtaks.map((subtaks) => {
+            return (
+              <Subtask
+                id={subtaks.id}
                 key={subtaks.id}
                 onInputChange={subtaskChangeHandler}
                 onDelate={delateSubtaskHandler}
-                />
-            )
-        })}
-        <button type="button" onClick={addSubtaskHandler}>+Add Subtask</button>
-
+              />
+            );
+          })}
+          <button type="button" onClick={addSubtaskHandler}>
+            +Add Subtask
+          </button>
         </div>
         <div className={classes["form-element"]}>
           <label htmlFor="select">Status</label>
-          <Select onChange={slectChangeHandler} value={selectValue} className={classes.select}>
+          <Select
+            onChange={slectChangeHandler}
+            value={selectValue}
+            className={classes.select}
+          >
             <MenuItem value={"todo"}>Todo</MenuItem>
             <MenuItem value={"doing"}>Doing</MenuItem>
             <MenuItem value={"done"}>Done</MenuItem>
