8d06bcc00b3cdf74e2da258118fdddc60a748726
Filip Krzemień <filipos2003@gmail.com>
2023-05-24 01:33:27 +0200

    Imrpoving page responsibility

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import SettingsIcon from '@mui/icons-material/Settings';
 import NewTask from '../NewTask/NewTask';
 import NewBoard from '../NewBoard/NewBoard';
 import classes from './Header.module.scss';
@@ -12,6 +13,24 @@ const Header = () => {
     const [isFormActive , setIsFormActive] = useState<boolean>(false);
     const [isNewTaskFormActive , setIsNewTaskFormActive] = useState<boolean>(false);
     const boardsNumber:number = useSelector((state : RootState) => state.board.boards.length);
+    const [windowWidth , setWindowwidth] = useState(window.innerWidth);
+    console.log(windowWidth);
+
+   useEffect(() => {
+        const handleResize = () => {
+          setWindowwidth(window.innerWidth);
+    }
+       
+            window.addEventListener('resize', handleResize)
+  
+        
+        return () => {
+            window.removeEventListener('resize' , handleResize);
+            
+        }
+      })
+
+
 
     const closeModalHandler = () => {
         setIsFormActive(false);
@@ -32,8 +51,9 @@ const Header = () => {
     return(
         <header className={classes.header}>
             <span>Kanban</span>
-            {boardsNumber > 0 && <button className={classes.adder} onClick={openTaskModalHandler}>+Add new Task</button>}
-            <button className={classes.adder} onClick={openModalHandler}>+Add new board</button>
+            {boardsNumber > 0 && <button className={classes.adder} onClick={openTaskModalHandler}>{windowWidth > 800 ? '+Add new Task' : '+Task'}</button>}
+            <button className={classes.adder} onClick={openModalHandler}>{windowWidth > 800 ? '+Add new Board' : '+Board'}</button>
+            {windowWidth < 600 && <SettingsIcon fontSize='large' className={classes.settings} />}
             {isFormActive && <Modal onClose={closeModalHandler}><NewBoard onClose={closeModalHandler}/></Modal>}
            { isNewTaskFormActive &&  <Modal onClose={closeTaskModalHandler}><NewTask onClose={closeTaskModalHandler}/></Modal> }
         </header>
